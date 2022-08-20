d5c9c9681d979c17adceeb448255cf3197f753b1
Sebastian W <Seb@seb.home>
2022-08-20 10:14:01 +0100

    1st commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+    return (
+        <header className="App-header">
+            <input placeholder='Look for a ship' type='text' />
+            <button type='submit'>Search</button>
+        </header>
+    )
+}
+
+export default Search;
