094a66dba733ef9af667ccf9d5b9a8ecdb6f894a
Sebastian W <sebwilczega@gmail.com>
2022-08-22 18:20:12 +0100

    fetching, not working tho

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search() {
+
+    const [searchShips, setSearchShips] = useState("")
+
+    const ships = async (e) => {
+        const response = await fetch(`http https://swapi.dev/api/starships/?search=${e.target.value.toLowerCase()}`)
+        const data = response.json();
+        e.target.value.toLowerCase()
+    
+        setSearchShips(data.result)
+        console.log(searchShips)
+    }
+
     return (
         <header className="App-header">
-            <input placeholder='Look for a ship' type='text' />
+            <input onChange={ships} placeholder='Look for a ship' type='text' />
         </header>
     )
 }
