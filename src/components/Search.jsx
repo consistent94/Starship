72a5be261c422c032d23e029b2df52fb95b8a564
Sebastian W <sebwilczega@gmail.com>
2022-08-29 13:37:34 +0100

    log the obcjects

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,39 +3,45 @@ import React, { useState } from "react";
 function Search() {
 
     const [searchShips, setSearchShips] = useState("")
-    const [showDropdown, setShowDropdown] = useState(true)
+    const [showDropdown, setShowDropdown] = useState(false)
     const [displayShip, setDisplayShip] = useState([])
 
     const ships = async (e) => {
-        const response = await fetch(`http https://swapi.dev/api/starships/?search=${e.target.value.toLowerCase()}`)
-        const data = response.json();
         e.target.value.toLowerCase()
+        const response = await fetch(`https://swapi.dev/api/starships/?search=${e.target.value.toLowerCase()}`)
+        const data = await response.json();
     
-        setSearchShips(data.results)
+        setSearchShips(data.results.name)
+        console.log(data);
         
     }
 
-    const listOfShips = searchShips.map((el) => (
+
+    const listOfShips = displayShip.map((el) => {
+      return (
         <div className="ships">
           <p>{el.name}</p>
           <button
             className="add-btn"
             onClick={() => {
               setDisplayShip((prev) => [...prev, el]);
-              setShowDropdown(false);
-            }}
+              setShowDropdown(true);
+            } }
           >
             +
           </button>
         </div>
-      ));
+      );
+    });
 
     return (
         <header className="App-header">
             <input onChange={ships} placeholder='Look for a ship' type='text' />
             { showDropdown ? (<div className="dropdown-content">{listOfShips}</div>) : null}
+            <p>{displayShip}</p>
         </header>
     )
+
 }
 
 export default Search;
